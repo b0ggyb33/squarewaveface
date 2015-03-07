f16b22e217d4f85cc019e207bab88ea4331a7e93
b0ggyb33 <b0ggyb33@gmail.com>
2015-03-07 12:18:41 +0000

    bitmpa loaded

--- src/feature_gpath.c
+++ src/feature_gpath.c
@@ -11,6 +11,9 @@ static Layer *s_canvas_layer;
 
 static int32_t hourAngle,minuteAngle,secondAngle;
 
+static GBitmap *s_example_bitmap;
+static BitmapLayer *s_bitmap_layer;
+
 GPoint getGPointFromAngle(GPoint center,int radius,int angle)
 {
   //secondHand.y = (-cos_lookup(second_angle) * secondHandLength / TRIG_MAX_RATIO) + center.y;
@@ -21,7 +24,8 @@ GPoint getGPointFromAngle(GPoint center,int radius,int angle)
   return edge;
 }
 
-static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+static void canvas_update_proc(Layer *this_layer, GContext *ctx) 
+{
   GRect bounds = layer_get_bounds(this_layer);
 
   unsigned int radius = 45;
@@ -32,6 +36,8 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   //HourCircle
   graphics_context_set_stroke_color(ctx, GColorMelon);
   graphics_draw_circle(ctx, center, radius);
+  graphics_draw_circle(ctx, center, radius-1);
+  graphics_draw_circle(ctx, center, radius+1);
   //Hour Line
   graphics_context_set_stroke_color(ctx, GColorRed);
   GPoint hourEdge = getGPointFromAngle(center,radius,hourAngle);
@@ -39,10 +45,12 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   
   //minute
   GPoint minuteEdge = getGPointFromAngle(hourEdge,radius/2,minuteAngle);
-  graphics_context_set_stroke_color(ctx, GColorGreen);
+  graphics_context_set_stroke_color(ctx, GColorLimerick);
   graphics_draw_line(ctx,hourEdge,minuteEdge);
-  graphics_context_set_stroke_color(ctx, GColorMintGreen);
+  graphics_context_set_stroke_color(ctx, GColorLimerick);
   graphics_draw_circle(ctx, hourEdge, radius/2);
+  graphics_draw_circle(ctx, hourEdge, radius/2+1);
+  graphics_draw_circle(ctx, hourEdge, radius/2-1);
   
   //second
   GPoint secondEdge = getGPointFromAngle(minuteEdge,radius/4,secondAngle);
@@ -50,24 +58,38 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   graphics_draw_line(ctx,minuteEdge,secondEdge);
   graphics_context_set_stroke_color(ctx, GColorPictonBlue);
   graphics_draw_circle(ctx, minuteEdge, radius/4);
-  
+  graphics_draw_circle(ctx, minuteEdge, radius/4+1);
+  graphics_draw_circle(ctx, minuteEdge, radius/4-1);
+    
 }
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
+  //create Background
+  s_bitmap_layer = bitmap_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_example_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_layer));
+  bitmap_layer_set_alignment(s_bitmap_layer, GAlignCenter);
+  bitmap_layer_mark_dirty(s_bitmap_layer);
   // Create Layer
   s_canvas_layer = layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
   layer_add_child(window_layer, s_canvas_layer);
 
   // Set the update_proc
   layer_set_update_proc(s_canvas_layer, canvas_update_proc);
+   
+
+  
 }
 
 static void main_window_unload(Window *window) {
   // Destroy Layer
   layer_destroy(s_canvas_layer);
+  //destroy background
+  gbitmap_destroy(s_example_bitmap);
+  bitmap_layer_destroy(s_bitmap_layer);
 }
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) 
 {
@@ -97,6 +119,9 @@ static void init(void) {
   hourAngle=0;
   minuteAngle=0;
   secondAngle=0;
+  
+  s_example_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WHITE_BCKGRND);
+  //144x168
   
 }
 
